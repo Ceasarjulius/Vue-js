c7061aa9442e3a365cae0ec8951720cb17d8f330
Ceasarjulius <sisajulius69@gmail.com>
2024-08-12 07:32:54 -0700

    the todo app made by vue

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,228 @@
-<script   setup>
-import { ref } from 'vue';
+<script setup>
+import { ref, onMounted, reactive } from "vue";
+import ListItem from "./components/ListItem.vue";
+import ListItemCard from "./components/ListItemCard.vue";
 
 
-    const getPrincipal = ref(0);
-    const getRate = ref(0);
-    const getTime = ref(0);
-    const timeUnit = ref('years'); // 'years' or 'months'
-    const result = ref(0);
+// Vue JS
+var taskInput = ref('');
 
-    function handleSubmit() {
-      const principal = parseFloat(getPrincipal.value);
-      const rate = parseFloat(getRate.value);
-      let time = parseFloat(getTime.value);
+var itemsArray = ref([]);
 
-      // Convert time to years if the selected unit is months
-      if (timeUnit.value === 'months') {
-        time = (time / 12);
-        // console.log(time)
-      }
+async function getItems() {
+    const response = await fetch('http://localhost:5001/items');
 
-      // Calculate interest
-      // console.log(time) // 1
-      // console.log(rate) // 10
-      // console.log(principal) // 100
+    itemsArray.value = await response.json();
+}
 
-      // console.log('The res', (principal * rate * time) / 100);
-      
-      result.value = (principal * rate * time) / 100;
-      // console.log(result.value)
-    }
+async function getItem(id){
+    const response = await fetch(`http://localhost:5001/items/${id}`);
+    const data = await response.json();
+    return data
+}
+
+var itemToEdit = ref(null);
+
+async function saveItem() {
+    if (itemToEdit.value !== null) {
+    //    Code to update
+        const body = {
+            title: taskInput.value
+        }
+
+        const response = await fetch(`http://localhost:5001/items/${itemToEdit.value}`, {
+            method: "PATCH",
+            headers: {
+                "content-type" : "application/json"
+            },
+            body: JSON.stringify(body)
+        });
+
+        itemToEdit.value = null;
+    } else {
+        const body = {
+            title: taskInput.value
+        }
+
+        const response = await fetch('http://localhost:5001/items', {
+            method: "POST",
+            headers: {
+                "content-type" : "application/json"
+            },
+            body: JSON.stringify(body)
+        });
+
+        console.log(response.json());
+        }
+        taskInput.value = '';
+}
+
+async function deleteItem(id) {
+    const response = await fetch(`http://localhost:5001/items/${id}`,{
+        method: "DELETE"
+    });
+}
+
+async function editItem(id) {
+    const item = await getItem(id);
+    itemToEdit.value = item.id
+    taskInput.value = item.title
+    
+//   taskInput.value = itemsArray.value[index];
+//   itemToEdit.value = index
+  // console.log(itemToEdit.value);
+  
+}
+
+
+onMounted( () => {
+    getItems();
+})
 
 
 </script>
 
 <template>
-  <div class="flex items-center m-12 border box-border w-max h-auto rounded-lg">
-    <div>
-      <form v-on:submit.prevent="handleSubmit">
-        <div class="m-4">
-          <label class="block mb-2">Principal:</label>
-          <input v-model="getPrincipal" type="number" name="principal" id="principal" class="border border-black rounded-md">
+
+    <div class="main-container">
+        <div class="container">
+            <h1>To Do App</h1>
+
+            <form v-on:submit.prevent="saveItem" id="input-form">
+                <input v-model="taskInput" type="text" name="input" id="input" placeholder="Enter your task here.....">
+                <button type="submit">
+                    <i class="fa-solid fa-plus"></i>
+                </button>
+            </form>
+
+            <hr>
+            <div style="width: 100%;">
+
+
+                <ListItemCard v-if="itemsArray.length > 0">
+                    <ListItem 
+                        v-for="(item) in itemsArray" 
+                        :key="item.id" 
+                        :itemName="item.title"
+                        @edit="editItem(item.id)" 
+                        @delete="deleteItem(item.id)" />
+                </ListItemCard>
+                <p v-else>
+                    You have nothing in your list
+                </p>
+
+                <!-- <ListItem></ListItem> -->
+
+
+                <!-- <div class="todo-list-item"  >
+                <div class="todo-list-item-name" >
+                    <p 
+                    class="todo-title" >
+                      {{ item }}
+                      {{ index }}
+                    </p>
+                </div>
+                <div>
+                    <button @click="editItem(index)" id="edit" class="icon edit">
+                        <i class="fa-solid fa-pencil"></i>
+                    </button>
+                    <button v-on:click="deleteItem(index)" id="delete" class="icon delete">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+                </div>
+            </div> -->
+
+
+
+
+                <!-- <div :class="className" v-for="(num, index) in nums" :key="index" >
+              {{ num }}
+
+            </div> -->
+
+            </div>
         </div>
-        <div class="m-4">
-          <label class="block mb-2">Rate:</label>
-          <input v-model="getRate" type="number" name="rate" id="rate" class="border border-black rounded-md">
-        </div>
-        <div class="m-4">
-          <label class="block mb-2">Time:</label>
-          <input v-model="getTime" type="number" name="time" id="time" class="border border-black rounded-md">
-          <select v-model="timeUnit" class="border border-black rounded-md ml-2">
-            <option value="years">Years</option>
-            <option value="months">Months</option>
-          </select>
-        </div>
-        <button id="submit" name="submit" type="submit" class="border border-sky-300 rounded-2xl px-2 py-1 cursor-pointer hover:bg-slate-300 ml-14">
-          Calculate
-        </button>
-      </form>
     </div>
-    <div class="bg-black p-32 m-5">
-      <h1 class="text-white">The interest is: {{ result }}</h1>
-    </div>
-  </div>
+
 </template>
+
+<style>
+* {
+    padding: 0;
+    margin: 0;
+}
+
+.main-container {
+    background-color: #3F72AF;
+    height: 100vh;
+    display: flex;
+    /* flex-direction: co; */
+    justify-content: center;
+    /* align horizontally */
+    align-items: center;
+    /* align vertically */
+
+}
+
+.container {
+    background-color: #fff;
+    width: 50%;
+    border-radius: 20px;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    /* align horizontally */
+}
+
+hr {
+    width: 100%;
+    margin: 30px 0;
+}
+
+h1 {
+    color: #112D4E;
+    font-size: 40px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    margin: 10px 0;
+}
+
+#input-form {
+    display: flex;
+
+}
+
+#input-form input {
+    border: 1px solid #B1B1B1;
+    padding: 10px 6px;
+    font-size: 22px;
+    border-radius: 5px;
+    border-top-right-radius: 0px;
+    border-bottom-right-radius: 0px;
+    background-color: #DBE2EF;
+}
+
+#input-form button {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    border-top-left-radius: 0px;
+    border-bottom-left-radius: 0px;
+    background-color: #AAAAAA;
+    cursor: pointer;
+}
+
+
+.icon {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    padding: 0px 10px;
+}
+
+
+
+.fa-trash {
+    color: red;
+}
+</style>
