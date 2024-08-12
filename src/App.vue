41b9667c64dcc5f5384b98d670a6c281946c34e8
Ceasarjulius <sisajulius69@gmail.com>
2024-08-11 23:53:49 -0700

    the calculator made with Vue

--- src/App.vue
+++ src/App.vue
@@ -1,216 +1,65 @@
-<script setup>
-import {ref} from "vue";
-// Vue JS
-var taskInput = ref('');
-
-var itemsArray = ref([
-]);
-
-// const items = [1,2,3,4,5,6,7,8];
-// const nums = ['one', 'two', 'three', 'four'];
-
-// // for (var i = 0; i <= arr.length - 1; i ++ ) {
-// //   console.log(arr[i]);
-// // }
+<script   setup>
+import { ref } from 'vue';
 
 
-// items.forEach( (item, index) => {
-//   console.log(item, index);
-// })
+    const getPrincipal = ref(0);
+    const getRate = ref(0);
+    const getTime = ref(0);
+    const timeUnit = ref('years'); // 'years' or 'months'
+    const result = ref(0);
 
-// JS
-// form.addEventListener('submit', saveItem);
-var itemToEdit = ref(null);
+    function handleSubmit() {
+      const principal = parseFloat(getPrincipal.value);
+      const rate = parseFloat(getRate.value);
+      let time = parseFloat(getTime.value);
 
+      // Convert time to years if the selected unit is months
+      if (timeUnit.value === 'months') {
+        time = (time / 12);
+        // console.log(time)
+      }
 
-function saveItem(){
-  // console.log(itemsArray.value);
-  // console.log(taskInput.value);
-  console.log('itemToEdit');
-  console.log(itemToEdit.value);
-  
-  if (itemToEdit.value !== null) {
-    itemsArray.value[itemToEdit.value] = taskInput.value;
-    itemToEdit.value = null;
-  } else {
-    itemsArray.value.push(taskInput.value);
-  }
-  taskInput.value = '';
+      // Calculate interest
+      // console.log(time) // 1
+      // console.log(rate) // 10
+      // console.log(principal) // 100
 
-  // console.log(itemsArray.value);
-  
-}
-
-function deleteItem(index){
-  itemsArray.value.splice(index, 1);
-}
-
-
-function editItem(index) {
-  taskInput.value = itemsArray.value[index];
-  itemToEdit.value = index
-  // console.log(itemToEdit.value);
-  
-}
+      // console.log('The res', (principal * rate * time) / 100);
+      
+      result.value = (principal * rate * time) / 100;
+      // console.log(result.value)
+    }
 
 
 </script>
 
 <template>
-
-<div class="main-container" >
-<div class="container" >
-        <h1>To Do App</h1>
-
-        <form v-on:submit.prevent="saveItem" id="input-form">
-            <input v-model="taskInput" type="text" name="input" id="input" placeholder="Enter your task here.....">
-            <button type="submit">
-                <i class="fa-solid fa-plus"></i>
-            </button>
-        </form>
-
-        <hr >
-        <div id="todo-list-items" >
-            <div class="todo-list-item" v-for="(item, index) in itemsArray" :key="index" >
-                <div class="todo-list-item-name" >
-                    <p 
-                    class="todo-title" >
-                      {{ item }}
-                      <!-- {{ index }} -->
-                    </p>
-                </div>
-                <div>
-                    <button @click="editItem(index)" id="edit" class="icon edit">
-                        <i class="fa-solid fa-pencil"></i>
-                    </button>
-                    <button v-on:click="deleteItem(index)" id="delete" class="icon delete">
-                        <i class="fa-solid fa-trash"></i>
-                    </button>
-                </div>
-            </div>
-
-
-
-            <!-- <div :class="className" v-for="(num, index) in nums" :key="index" >
-              {{ num }}
-
-            </div> -->
-    
+  <div class="flex items-center m-12 border box-border w-max h-auto rounded-lg">
+    <div>
+      <form v-on:submit.prevent="handleSubmit">
+        <div class="m-4">
+          <label class="block mb-2">Principal:</label>
+          <input v-model="getPrincipal" type="number" name="principal" id="principal" class="border border-black rounded-md">
         </div>
+        <div class="m-4">
+          <label class="block mb-2">Rate:</label>
+          <input v-model="getRate" type="number" name="rate" id="rate" class="border border-black rounded-md">
+        </div>
+        <div class="m-4">
+          <label class="block mb-2">Time:</label>
+          <input v-model="getTime" type="number" name="time" id="time" class="border border-black rounded-md">
+          <select v-model="timeUnit" class="border border-black rounded-md ml-2">
+            <option value="years">Years</option>
+            <option value="months">Months</option>
+          </select>
+        </div>
+        <button id="submit" name="submit" type="submit" class="border border-sky-300 rounded-2xl px-2 py-1 cursor-pointer hover:bg-slate-300 ml-14">
+          Calculate
+        </button>
+      </form>
+    </div>
+    <div class="bg-black p-32 m-5">
+      <h1 class="text-white">The interest is: {{ result }}</h1>
     </div>
   </div>
-
 </template>
-
-<style>
-
-* {
-    padding: 0;
-    margin: 0;
-}
-
-.main-container{
-    background-color: #3F72AF;
-    height: 100vh;
-    display: flex;
-    /* flex-direction: co; */
-    justify-content: center;  /* align horizontally */
-    align-items: center; /* align vertically */
-
-}
-
-.container{
-    background-color: #fff;
-    width: 50%;
-    border-radius: 20px;
-    padding: 40px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;  /* align horizontally */
-}
-
-hr{
-    width: 100%;
-    margin: 30px 0;
-}
-
-h1 {
-    color: #112D4E;
-    font-size: 40px;
-    font-family:Verdana, Geneva, Tahoma, sans-serif;
-    margin: 10px 0;
-}
-
-#input-form {
-    display: flex;
-
-}
-
-#input-form input{
-    border: 1px solid #B1B1B1;
-    padding: 10px 6px;
-    font-size: 22px;
-    border-radius: 5px;
-    border-top-right-radius: 0px;
-    border-bottom-right-radius: 0px;
-    background-color: #DBE2EF;
-}
-
-#input-form button{
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    border-top-left-radius: 0px;
-    border-bottom-left-radius: 0px;
-    background-color: #AAAAAA;
-    cursor: pointer;
-}
-
-#todo-list-items {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-}
-
-.todo-list-item{
-    background-color: #DBE2EF;
-    border-radius: 5px;
-    padding: 10px 20px;
-    display: flex;
-    justify-content: space-between;
-
-}
-
-.todo-list-item-name{
-    display: flex;
-    align-items: center;
-    gap: 20px;
-
-}
-
-.checkbox{
-    height: 20px;
-    width: 20px;
-}
-
-.todo-title{
-    font-size: 22px;
-}
-
-.icon{
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-    padding: 0px 10px;
-}
-
-
-
-.fa-trash{
-    color: red;
-}
-
-
-
-</style>
